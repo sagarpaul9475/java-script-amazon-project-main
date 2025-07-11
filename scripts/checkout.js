355624416f591c3bd96571394c19bfe4baf616f5
Sagar Paul <SagarPaul9475@gmail.com>
2025-07-12 00:23:04 +0530

    convert products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import "../data/cart-class.js";//Runs the data directly.
+//import "../data/cart-class.js";//Runs the data directly.
 
 renderOrderSummary();
 renderPaymentSummary();
