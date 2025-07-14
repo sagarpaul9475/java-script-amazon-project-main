cc3df728bbf1b57c1b1e95878e1e90f535e2ce32
Sagar Paul <sagarpaul9475@gmail.com>
2025-07-14 12:02:34 +0530

    This is a check

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,7 @@
 import { cart, AddToCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from "./utils/money.js";
-
+//checking
 document.addEventListener('DOMContentLoaded', () => {
   let ProductHtml = '';
 
